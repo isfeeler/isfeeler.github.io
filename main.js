5608952d1fa750f6d471a68da048e55ca8484b43
Svatko Igor <isfeeler@gmail.com>
2024-05-09 23:20:25 +0700

    Update main.js

--- main.js
+++ main.js
@@ -129,7 +129,7 @@ input.addEventListener('keyup', handleChange);
 //   console.log(iti.getNumber())
 // }
 
-send_data = basket + iti.getNumber();
+send_data = iti.getNumber();
 
 Telegram.WebApp.onEvent("mainButtonClicked", function(){
   tg.sendData(send_data);
